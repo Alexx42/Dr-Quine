f3c998f2a199a6409237597b5389ded576f1780e
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-26 19:37:09 -0700

    add grace

--- C/Grace.c
+++ C/Grace.c
@@ -1,19 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Grace.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/26 15:50:27 by ale-goff          #+#    #+#             */
-/*   Updated: 2019/06/26 15:52:27 by ale-goff         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+/*
+** COMMENTAIRE
+*/
+#define FILE "Grace_kid.c"
+#define SELF "#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%c/*%c** COMMENTAIRE%c*/%c#define FILE %cGrace_kid.c%c%c#define SELF %c%s%c%c#define MAIN int main() {int fd; if ((fd = open(FILE, O_CREAT | O_WRONLY | O_TRUNC)) > 0){dprintf(fd, SELF, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, SELF, 34, 10, 10, 10, 10);}}%c%cMAIN%c"
+#define MAIN int main() {int fd; if ((fd = open(FILE, O_CREAT | O_WRONLY | O_TRUNC)) > 0){dprintf(fd, SELF, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, SELF, 34, 10, 10, 10, 10);}}
 
-int			main(void)
-{
-
-	return (0);
-}
+MAIN
