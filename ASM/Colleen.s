090ace6c680241f3059048c578d9bf857a81ee68
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-27 19:26:42 -0700

    Colleen ASM done

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,18 +1,21 @@
 ; commentary outside
 section .data
-msg: db "; commentary outside%2$csection .data%2$c%2$csection .text%2$cglobal _main%2$cextern _printf", 0
+	msg db "; commentary outside%2$csection .data%2$c%4$cmsg db %3$c%5$s%3$c, 0%2$csection .text%2$c%4$cglobal start%2$c%4$cglobal _main%2$c%4$cextern _printf%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp,rsp%2$c%4$cmov rdx,10%2$c%4$cmov rcx,34%2$c%4$cmov r8,9%2$c%4$clea r9,[rel msg]%2$c%4$clea rdi,[rel msg]%2$c%4$ccall _printf%2$c%4$cleave ;equivalent of pop%2$c%4$cret%2$c", 0
 section .text
-global _main
-extern _printf
+	global start
+	global _main
+	extern _printf
+start:
+	call _main
+	ret
 _main:
-	; commentary inside
 	push rbp
-	lea rdi, [rel msg]
-	lea rsi, [rel msg]
-	mov rdx, 10
-	mov rcx, 34
-	mov r8, 9
+	mov rbp,rsp
+	mov rdx,10
+	mov rcx,34
+	mov r8,9
+	lea r9,[rel msg]
+	lea rdi,[rel msg]
 	call _printf
-	pop rbp
-	mov rax, 0
+	leave ;equivalent of pop
 	ret
