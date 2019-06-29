47342ec9a846d091a4d75b9c04f65e7f718bf288
Alexandre Le Goff <legoff.alexandre06@gmail.com>
2019-06-28 20:27:04 -0700

    Grace done

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,6 +1,6 @@
 ; commentary outside
 section .data
-	msg db "; commentary outside%2$csection .data%2$c%4$cmsg db %3$c%5$s%3$c, 0%2$csection .text%2$c%4$cglobal start%2$c%4$cglobal _main%2$c%4$cextern _printf%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp,rsp%2$c%4$cmov rdx,10%2$c%4$cmov rcx,34%2$c%4$cmov r8,9%2$c%4$clea r9,[rel msg]%2$c%4$clea rdi,[rel msg]%2$c%4$ccall _printf%2$c%4$cleave ;equivalent of pop%2$c%4$cret%2$c", 0
+	msg db "; commentary outside%2$csection .data%2$c%4$cmsg db %3$c%5$s%3$c, 0%2$csection .text%2$c%4$cglobal start%2$c%4$cglobal _main%2$c%4$cextern _printf%2$cstart:%2$c%4$ccall _main%2$c%4$cret%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp,rsp%2$c%4$cmov rdx,10%2$c%4$cmov rcx,34%2$c%4$cmov r8,9%2$c%4$clea r9,[rel msg]%2$c%4$clea rdi,[rel msg]%2$c%4$ccall _printf%2$c%4$cleave ;equivalent of pop%2$c%4$cmov rax,0%2$c%4$cret%2$c", 0
 section .text
 	global start
 	global _main
@@ -18,4 +18,5 @@ _main:
 	lea rdi,[rel msg]
 	call _printf
 	leave ;equivalent of pop
+	mov rax,0
 	ret
