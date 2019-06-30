b05756a3c53b4f333c43e13b44628bfc4cb50a19
Alexandre Le goff <ale-goff@e1z2r5p31.42.us.org>
2019-06-30 16:23:49 -0700

    Colleen in PYTHON

---
+++ PYTHON/Colleen.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#commentary
+if __name__ == "__main__":
+	#commentary 2
+	a = "#!/usr/bin/python3%c#commentary%cif __name__ == %c__main__%c:%c%c#commentary 2%c%ca = %c%s%c; print%ca %% %c10, 10, 34, 34, 10, 9, 10, 9, 34, a, 34, 40, 40))"; print(a % (10, 10, 34, 34, 10, 9, 10, 9, 34, a, 34, 40, 40))
